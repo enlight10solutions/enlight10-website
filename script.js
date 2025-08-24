abd16ec41fb2a9e69708028d7c253d068fc928bb
enlight10solutions <Jay.Moreno@enlight10solutions.com>
2025-08-23 21:23:15 -1000

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// js/script.js
+document.addEventListener("DOMContentLoaded", function() {
+  const btn = document.querySelector("button");
+  if (btn) {
+    btn.addEventListener("click", () => {
+      alert("Thanks for visiting enLight10 Solutions!");
+    });
+  }
+});
